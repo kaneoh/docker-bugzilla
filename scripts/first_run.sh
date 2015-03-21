627586266783149ea3d8e3c65f661e2c3f136fc6
Zhiguang Li <muzili@gmail.com>
2015-03-21 10:04:52 +0800

    Correct the database
    Change-Id: I57d5f99c6b092d2adede5de9c2c46f07eca8c4b6
    Signed-off-by: Zhiguang Li <muzili@gmail.com>

--- scripts/first_run.sh
+++ scripts/first_run.sh
@@ -136,6 +136,10 @@ EOF
 }
 
 check_mysql() {
+  DB_USER=${DB_USER:-bugs}
+  DB_PASS=${DB_PASS:-CaP8VcETXUv2g}
+  DB_NAME=${DB_NAME:-bugs}
+
   echo "mysql: $MYSQL_ENV_USER:$MYSQL_ENV_PASS@$MYSQL_PORT_3306_TCP_ADDR:$MYSQL_PORT_3306_TCP_PORT"
   RET=1
   TIMEOUT=0
@@ -157,7 +161,8 @@ check_mysql() {
     RET=$?
     echo "mysql status is $RET"
   done
-    echo "starting installation"
+
+  echo "starting installation"
   if [ -z "$MYSQL_ENV_PASS" ]; then
       echo "no linked mysql detected"
   else
@@ -172,14 +177,26 @@ check_mysql() {
       echo "set MySQL user default to: $MYSQL_ENV_USER"
       MYSQL_USER=$MYSQL_ENV_USER
   fi
-      cat >> /home/bugzilla/localconfig <<EOL
+
+  echo "Creating database \"$db\"..."
+  mysql -u$MYSQL_ENV_USER -p$MYSQL_ENV_PASS \
+        -h$MYSQL_PORT_3306_TCP_ADDR \
+        -P$MYSQL_PORT_3306_TCP_PORT \
+        -e "CREATE DATABASE IF NOT EXISTS '${DB_NAME}' DEFAULT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci';"
+  echo "Granting access to database \"${DB_NAME}\" for user \"${DB_USER}\"..."
+  mysql -u$MYSQL_ENV_USER -p$MYSQL_ENV_PASS \
+        -h$MYSQL_PORT_3306_TCP_ADDR \
+        -P$MYSQL_PORT_3306_TCP_PORT \
+        -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}' IDENTIFIED BY '${DB_PASS}';"
+
+  cat >> /home/bugzilla/localconfig <<EOL
 \$db_driver = 'mysql';
 \$db_host = '$MYSQL_HOST';
-\$db_name = 'bugs';
-\$db_user = '$MYSQL_USER';
-\$db_pass = '$MYSQL_ENV_PASS';
-\$db_port = '$MYSQL_ENV';
-\$db_sock = '$MYSQL_PORT_3306_TCP_PORT';
+\$db_name = '$DB_NAME';
+\$db_user = '$DB_USER';
+\$db_pass = '$DB_PASS';
+\$db_port = '$MYSQL_PORT_3306_TCP_PORT';
+\$db_sock = '';
 \$db_check = 1;
 \$db_mysql_ssl_ca_file = '';
 \$db_mysql_ssl_ca_path = '';
