61847a6107a20ae054fb08ce25868aefe4ad5fd4
Zhiguang Li <muzili@gmail.com>
2015-05-09 16:19:13 +0800

    Fix the bugzilla group
    Change-Id: Ib6cda63e611bfbf4b85a5d7e33240a123d49f426
    Signed-off-by: Zhiguang Li <muzili@gmail.com>

--- scripts/first_run.sh
+++ scripts/first_run.sh
@@ -64,7 +64,7 @@ EOF
 \$answer{'memcached_servers'} = "localhost:11211";
 \$answer{'urlbase'} = '$VIRTUAL_HOST';
 \$answer{'use_suexec'} = '';
-\$answer{'webservergroup'} = 'bugzilla';
+\$answer{'webservergroup'} = 'apache';
 EOF
 
 }
